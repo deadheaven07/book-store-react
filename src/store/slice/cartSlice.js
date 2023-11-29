23e8e28a5c5a55f9f8c380f30033bb99c0eaa746
Shobhit Umrao <shobhitumrao5@gmail.com>
2023-11-29 18:29:38 +0530

    cart slice complete

---
+++ src/store/slice/cartSlice.js
@@ -0,0 +1,76 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const localStorageCartKey = "_local_cart_";
+
+const loadCartFromLocalStorage = () => {
+  try {
+    const cartData = localStorage.getItem(localStorageCartKey);
+    return cartData ? JSON.parse(cartData) : { items: [], totalQuantity: 0 };
+  } catch (error) {
+    console.error("Error loading cart from localStorage:", error);
+    return { items: [], totalQuantity: 0 };
+  }
+};
+
+const saveCartToLocalStorage = (cart) => {
+  try {
+    const cartData = JSON.stringify(cart);
+    localStorage.setItem(localStorageCartKey, cartData);
+  } catch (error) {
+    console.error("Error saving cart to localStorage:", error);
+  }
+};
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState: loadCartFromLocalStorage(),
+  reducers: {
+    addItemToCart(state, action) {
+      const newBook = action.payload;
+      const existingBook = state.items.find(
+        (item) =>
+          item.id.toLowerCase().toString() ===
+          newBook.id.toLowerCase().toString()
+      );
+
+      // console.log(existingItem.quantity);
+
+      if (existingBook && existingBook.quantity) {
+        // console.log("Control reached");
+        // console.log(existingItem.quantity, existingItem.maxQuantity);
+        if (existingBook.quantity < existingBook.maxQuantity) {
+          // console.log("Control reached here");
+          existingBook.quantity += 1;
+          state.totalQuantity += 1;
+        }
+      } else {
+        state.items.push({ ...newBook, quantity: 1 });
+        state.totalQuantity += 1;
+      }
+
+      saveCartToLocalStorage(state);
+    },
+
+    removeItemFromCart(state, action) {
+      const bookId = action.payload;
+
+      const existingBook = state.items.find(
+        (item) =>
+          item.id.toLowerCase().toString() === bookId.toLowerCase().toString()
+      );
+
+      if (existingBook) {
+        if (existingBook.quantity > 1) {
+          existingBook.quantity -= 1;
+        } else {
+          state.items = state.items.filter(
+            (item) =>
+              item.id.toLowerCase().toString() !==
+              existingBook.id.toLowerCase().toString()
+          );
+        }
+      }
+      saveCartToLocalStorage(state);
+    },
+  },
+});
